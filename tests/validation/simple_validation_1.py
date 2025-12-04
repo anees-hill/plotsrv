5649b97d3d0b17737ed0b5983dfa76059e6633be
Samuel Anees-Hill <106348882+anees-hill@users.noreply.github.com>
2025-12-04 21:47:18 +0000

    chore(tablesrv): add df dependencies
    - also updates simple_validation script

--- tests/validation/simple_validation_1.py
+++ tests/validation/simple_validation_1.py
@@ -1,26 +1,79 @@
-import time
+# * [dev] dependencies are needed to run this script
 
-### PART 1
-# matplotib/seaborn
-from plotsrv.server import start_plot_server, stop_plot_server, refresh_plot_server
+import time
 import matplotlib.pyplot as plt
 import seaborn as sns
+from plotnine import *
+import polars as pl
+from plotsrv.server import (
+    start_plot_server,
+    stop_plot_server,
+    refresh_plot_server,
+    set_table_view_mode,
+)
+
+# PLOTS ----------------------
+### PART 1
+# matplotib/seaborn
+
+
 start_plot_server()
-dat = sns.load_dataset('titanic') 
+dat = sns.load_dataset("titanic")
 sns.scatterplot(data=dat, x="age", y="fare")
 plt.show()
-print('plot 1 (matplotlib) up')
+print("plot 1 (matplotlib) up")
 time.sleep(10)
 stop_plot_server()
 
 ### PART 2
 # plotnine
-from plotnine import *
-start_plot_server() 
-p=(ggplot(dat, aes("age", "fare")) + geom_point()) 
+start_plot_server()
+p = ggplot(dat, aes("age", "fare")) + geom_point()
 refresh_plot_server(p)
-print('plot 2 (plotnine) up')
-time.sleep(10) 
+print("plot 2 (plotnine) up")
+time.sleep(10)
 
 stop_plot_server()
-print('script finished')
+print("script finished")
+
+# TABLES ----------------------
+
+# * Simple-view ---------
+set_table_view_mode("simple")
+
+# pandas
+start_plot_server()
+df = sns.load_dataset("titanic")
+refresh_plot_server(df)
+print("df 1 (pandas) up - simple view")
+time.sleep(10)
+stop_plot_server()
+
+# pandas
+start_plot_server()
+df = sns.load_dataset("titanic")
+df = pl.from_pandas(df)
+refresh_plot_server(df)
+print("df 2 (polars) up - simple view")
+time.sleep(10)
+stop_plot_server()
+
+# * Rich-view ---------
+set_table_view_mode("rich")
+
+# pandas
+start_plot_server()
+df = sns.load_dataset("titanic")
+refresh_plot_server(df)
+print("df 1 (pandas) up - rich view")
+time.sleep(10)
+stop_plot_server()
+
+# polars
+start_plot_server()
+df = sns.load_dataset("titanic")
+df = pl.from_pandas(df)
+refresh_plot_server(df)
+print("df 2 (polars) up - rich view")
+time.sleep(10)
+stop_plot_server()
